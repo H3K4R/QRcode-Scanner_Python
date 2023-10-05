32e0bdf425e5b14ce4cc017a37f2f388ff5d3314
Sujit-Baram <109032729+Sujit-Baram@users.noreply.github.com>
2023-10-05 11:56:46 +0530

    Update app.py 2-3 line added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pyzbar import pyzbar
-
-
+import argparse
+import cv2
 ap=argparse.ArgumentParser()
 ap.add_argument("-i","--image",required=True,help="Path to input image")
 args=vars(ap.parse_args())
